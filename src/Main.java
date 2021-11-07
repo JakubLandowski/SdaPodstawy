4dce4a4483a6b05ab955a1fd354dabab8195b877
Jakub Landowski <landowski222@gmail.com>
2021-11-07 12:58:41 +0100

    Sum commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     //TODO
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.print("Hello world\n");
+        System.out.println("BBbbBBbbBB");
+
+        int b = 5;
+        int a = 10;
+        int c = Main.sum(a,b);
+
+        System.out.println(a + "+" + b + "=" + c);
+    }
+
+     public static int sum(int a, int b){
+        int c = a+b;
+        return c;
     }
 }

2f40f403161274a274c53fd8706052bf331d4aba
Jakub Landowski <landowski222@gmail.com>
2021-11-07 14:15:37 +0100

    String commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,33 @@
 public class Main {
-    //TODO
+    //TODO//komentarz do todo
 
     public static void main(String[] args) {
+        /*
         System.out.print("Hello world\n");
         System.out.println("BBbbBBbbBB");
 
         int b = 5;
-        int a = 10;
+        int a = 12;
 
         System.out.println(a + "+" + b + "=" + Main.sum(a,b));
+        System.out.println(a % b); // modulo
+        */  //   method sum()
+
+        String account = " Janusz Tracz ";
+        System.out.println(account.length());
+        System.out.println(account.indexOf('a'));
+        System.out.println(account.substring(0,7));
+        System.out.println(account);
+        System.out.println(account.trim());
+
+        char A = 'A';
+        char alpha = '\u03B1';
+        System.out.println(alpha);
+
+        char[] hello = {'h','e','l','l','o'};
+        System.out.println(hello);
+        System.out.println(Character.isDigit(A));
+
     }
 
      public static int sum(int a, int b){
